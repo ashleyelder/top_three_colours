db06cb76e64f2b257e8ef17794289d70f20a2331
Ashley Elder <shlee.elder@gmail.com>
2019-11-24 15:34:11 -0700

    adds proper test for export CSV

--- main_test.go
+++ main_test.go
@@ -1,19 +1,37 @@
-// package main
+package main
 
-// import "testing"
+import "testing"
+import "os"
+import "io/ioutil"
+import "fmt"
 
-// import "bytes"
+// example test helper that creates temp file - really should be used for very reused logic that doesn't fail often - easier to trace if logic within the test itself insted of abstraction
+// should never return an error, accesses T structures so fails the test if there is an error
+func testTempFile(t *testing.T) (*os.File, func()) {
+	// improves stack trace output better during a panic situation in test helper
+	t.Helper()
 
-// func TestWriteCSV(t *testing.T) {
+	tf, err := ioutil.TempFile("", "test")
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
 
-// 	var b bytes.Buffer
-// 	b.WriteString("fake, csv, data")
+	tf.Close()
 
-// buffer cannot be used as am argument to os.File because it doesn't have a file descriptor, a number managed by the os
-// content, err := writeCSV(&b)
-// if err != nil {
-// 	t.Error("Failed to read csv data")
-// }
-// fmt.Print(content)
-// }
+	// as cleanup returns closure that deletes the temp file
+	return tf, func() { os.Remove(tf.Name()) }
+}
 
+func TestWriteCSV(t *testing.T) {
+	tf, tfclose := testTempFile(t)
+
+	defer tfclose()
+
+	writeCSV(tf)
+
+	content, err := writeCSV(tf)
+	if err != nil {
+		t.Error("Failed to read csv data")
+	}
+	fmt.Print(content)
+}
