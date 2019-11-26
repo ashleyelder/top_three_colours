fa456f8185308b1331d376e4a0e45191e4372323
ashleyelder <shlee.elder@gmail.com>
2019-11-25 23:27:08 -0700

    creates chan for urls and results, creates only one line item currently

--- main.go
+++ main.go
@@ -3,22 +3,24 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"encoding/csv"
+	// // "encoding/csv"
 	"fmt"
+	"github.com/EdlinOrg/prominentcolor"
 	"image"
 	_ "image/jpeg"
-	// "log"
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/EdlinOrg/prominentcolor"
+	// "time"
 )
 
-var results []string
+// makes global because difficulty accessing in readUrls and then processUrls (which isn't called from readUrls)
+var urls = make(chan string)
 
-func readUrls(fileInput string, urls []string) {
+func readUrls(fileInput string, results chan string) {
+	fmt.Println("readUrls")
 	var b bytes.Buffer
+
 	f, err := os.Open(fileInput)
 
 	checkError(&b, err)
@@ -29,24 +31,30 @@ func readUrls(fileInput string, urls []string) {
 
 	for scanner.Scan() {
 		url := scanner.Text()
-		urls = append(urls, url)
+		urls <- url
 	}
+}
 
-	for i := 0; i < len(urls); i++ {
+func processUrls(results chan string) {
+	fmt.Println("processUrls")
+	var b bytes.Buffer
 
-		img, err := loadImage(urls[i])
+	for url := range urls {
+		fmt.Println(url)
+		img, err := loadImage(url)
 
 		if err != nil {
 			checkError(&b, err)
+			fmt.Println(img)
 			continue
 		}
 
-		getThreePrevalentColours(img, urls[i])
-
+		getThreePrevalentColours(img, url, results)
 	}
 }
 
 func loadImage(url string) (image.Image, error) {
+	fmt.Println("loadImage")
 	var b bytes.Buffer
 	response, err := http.Get(url)
 
@@ -61,52 +69,54 @@ func loadImage(url string) (image.Image, error) {
 	return img, nil
 }
 
-func getThreePrevalentColours(image image.Image, url string) {
+func getThreePrevalentColours(image image.Image, url string, results chan string) {
+	fmt.Println("getThreePrevalentColours")
 	var b bytes.Buffer
 	colours, err := prominentcolor.Kmeans(image)
 
 	// pass reference to buffer instead of buffer itself
 	checkError(&b, err)
 
-	assembleLineItem(colours, url)
+	assembleLineItem(colours, url, results)
 }
 
-func assembleLineItem(colours []prominentcolor.ColorItem, url string) {
-	var result strings.Builder
-	result.WriteString(url)
+func assembleLineItem(colours []prominentcolor.ColorItem, url string, results chan string) {
+	fmt.Println("assembleLineItem")
 
+	// build a line item
+	var str strings.Builder
+	str.WriteString(url)
 	for _, colour := range colours {
-		result.WriteString(",#" + colour.AsString())
+		str.WriteString(",#" + colour.AsString())
 	}
-	result.WriteString("\n")
+	str.WriteString("\n")
+	lineItem := str.String()
 
-	s := result.String()
-
-	results = append(results, s)
+	results <- lineItem
 }
 
-func createAndWriteCSV() {
-	var b bytes.Buffer
-	file, err := os.Create("result.csv")
+// func createAndWriteCSV(results chan []string) {
+// 	var b bytes.Buffer
+// 	file, err := os.Create("result.csv")
 
-	checkError(&b, err)
-	defer file.Close()
+// 	checkError(&b, err)
+// 	defer file.Close()
 
-	writeCSV(file)
-}
+// 	writeCSV(file, results)
+// }
 
-func writeCSV(file *os.File) (message string, err error) {
-	var b bytes.Buffer
-	writer := csv.NewWriter(file)
-	defer writer.Flush()
+// func writeCSV(file *os.File, results chan []string) (message string, err error) {
+// 	var b bytes.Buffer
+// 	writer := csv.NewWriter(file)
+// 	defer writer.Flush()
 
-	for index, lineItem := range results {
-		fmt.Println(index, "=>", lineItem)
-		_, err := file.WriteString(lineItem)
-		checkError(&b, err)
-	}
-	return message, nil
-}
+// 	for index, lineItem := range results {
+// 		fmt.Println(index, "=>", lineItem)
+// 		_, err := file.WriteString(lineItem)
+// 		checkError(&b, err)
+// 	}
+// 	return message, nil
+// }
 
 func checkError(writer *bytes.Buffer, err error) (message string, error error) {
 	if err != nil {
@@ -116,12 +126,17 @@ func checkError(writer *bytes.Buffer, err error) (message string, error error) {
 }
 
 func main() {
-	var filename = "input.txt"
+	res := make(chan string)
 
-	// creates dynamic array
-	var links []string
+	filename := "input.txt"
 
-	readUrls(filename, links)
+	go readUrls(filename, res)
+	go processUrls(res)
 
-	createAndWriteCSV()
+	s := <-res
+	fmt.Println("s", s)
+
+	fmt.Scanln()
+
+	// createAndWriteCSV(results)
 }
