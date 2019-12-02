3dc4fd73238591b1c2248f11ad954aa5e87cf601
ashleyelder <shlee.elder@gmail.com>
2019-12-02 11:26:55 +0100

    adds comments for greater readability and refactor done channel to another wait group

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/EdlinOrg/prominentcolor"
 	"image"
 	_ "image/jpeg"
+	_ "image/png"
 	"net/http"
 	"os"
 	"strings"
@@ -15,8 +16,12 @@ import (
 	"time"
 )
 
-var urls = make(chan string, 10)
-var results = make(chan string, 10)
+// two bidirectionsl channels so they can send and receive data
+// urls will be sent to workers and results collected
+// sends to buffered channel are blocked when buffer is full - it is possible to write 10 urls into urls channel without being blocked
+// receives from buffered channel are blocked only when buffer is empty
+var urls = make(chan string, 20)
+var results = make(chan string, 20)
 
 func readUrls(fileInput string) {
 	var b bytes.Buffer
@@ -31,11 +36,15 @@ func readUrls(fileInput string) {
 
 	for scanner.Scan() {
 		url := scanner.Text()
+		// data sent to urls channel, writes are blocked until the worker goroutine reads from this channel
 		urls <- url
+		fmt.Println("successfully wrote", url, "to channel")
 	}
+	// close urls channel to notify receivers that no more data will be sent on the channel
 	close(urls)
 }
 
+// returns actual image from the url
 func loadImage(url string) (image.Image, error) {
 	var b bytes.Buffer
 	response, err := http.Get(url)
@@ -55,7 +64,7 @@ func getThreePrevalentColours(image image.Image, url string) {
 	var b bytes.Buffer
 	colours, err := prominentcolor.Kmeans(image)
 
-	// pass reference to buffer instead of buffer itself
+	// pass buffer by pointer
 	checkError(&b, err)
 	assembleLineItem(colours, url)
 }
@@ -84,8 +93,10 @@ func createAndWriteCSV(done chan bool) {
 }
 
 func writeCSV(file *os.File, done chan bool) (message string, err error) {
+	fmt.Println("writeCSV executed")
 	var b bytes.Buffer
 	writer := csv.NewWriter(file)
+	// writer.flush - does it stop writing the file after all lineItems are written?
 	defer writer.Flush()
 
 	for lineItem := range results {
@@ -98,18 +109,24 @@ func writeCSV(file *os.File, done chan bool) (message string, err error) {
 	return message, nil
 }
 
+// frequent creation of workers receive tasks on the url channel
 func worker(wg *sync.WaitGroup) {
 	var b bytes.Buffer
+	// a defer function to recover is a good idea here
+	// because any panics would otherwise crash the entire program
+	defer wg.Done()
+
+	// pull urls from queue until it's done/closed
+	// no need to check if channel is closed with ok variable
 	for url := range urls {
+		fmt.Println("concurrently read value", url, "from channel")
 		img, err := loadImage(url)
 		if err != nil {
 			checkError(&b, err)
 			continue
 		}
-
 		getThreePrevalentColours(img, url)
 	}
-	wg.Done()
 }
 
 func createWorkerPool(noOfWorkers int) {
@@ -118,10 +135,16 @@ func createWorkerPool(noOfWorkers int) {
 		wg.Add(1)
 		go worker(&wg)
 	}
+
+	// wait for the results to complete
 	wg.Wait()
+
+	// signal that we are done collecting results
+	// close the channel to notify receivers that no more data will be sent on the channel
 	close(results)
 }
 
+// helper function to print any error
 func checkError(writer *bytes.Buffer, err error) (message string, error error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error %s\n", err.Error())
@@ -137,12 +160,23 @@ func main() {
 	done := make(chan bool)
 	go createAndWriteCSV(done)
 
-	noOfWorkers := 10
+	// control the number of concurrently running tasks - hardcoded to be 10
+	// number is tuned to computing resources available, adjustable to optimize performance
+	noOfWorkers := 20
 	createWorkerPool(noOfWorkers)
 
+	// receive data from the done bool channel, does not use or store it in a variable, which is legal
+	// blocking line of code: until a goroutine writes data to the done channel, the control will not move to the next line of code
+	// main goroutine is blocked since it is waiting for data from the done channel below
 	<-done
 
 	endTime := time.Now()
 	diff := endTime.Sub(startTime)
 	fmt.Println("total time taken ", diff.Seconds(), "seconds")
 }
+
+// try adding second wait group for done
+// try to make call to checkError concurrent: https://golangbot.com/channels/ - "We will move that code to its own function and call it concurrently..." - does this make my program run faster?
+// try playing around with the buffer size - trying removing both of them
+// how come we dont put go in front of createWorkerPool? is it because it calls its own goroutine worker, that waits for 10 urls to process
+// when does a deadlock occur? when another goroutine isnt reading the channel of buffered value, or if a goroutine is trying to read but its not written i think...buffered channel cannot exceed capacity or deadlock
